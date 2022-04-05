a7f7651223c3a8671e9795e92ab2d21f56b8c11c
lamngo13 <lamngo@live.unc.edu>
2022-04-05 14:35:56 -0400

    trying to read input

--- flips.js
+++ flips.js
@@ -1,16 +1,20 @@
 import {coinFlip, coinFlips, countFlips, flipACoin} from "./modules/coin.mjs"
-import min from 'minimist'
+import minimist from 'minimist'
 
-let args = min(process.argv.slice(2));
-args['number'];
+let args = minimist(process.argv.slice(2));
 let theNum = args.number;
+//args['number'];
+console.log(theNum);
+//args['theNum'];
+//let theNum = args.number;
 
-if (theNum == null) {
-    let flip = coinFlip(1);
-    console.log(flip);
-    console.log(countFlips(flip));
-} else {
-    let flips = coinFlips(theNum);
-    console.log(flips);
-    console.log(countFlips(flips))
-}
+
+//if (theNum == null) {
+   // let flip = coinFlip(1);
+   // console.log(flip);
+  //  console.log(countFlips(flip));
+//} else {
+    //let flips = coinFlips(theNum);
+    //console.log(flips);
+  //  console.log(countFlips(flips))
+//}
