07f570929cb327400123c83e5b62736ca675bf90
lamngo13 <lamngo@live.unc.edu>
2022-04-05 15:04:04 -0400

    rip minimist?????

--- flips.js
+++ flips.js
@@ -1,4 +1,5 @@
 import {coinFlip, coinFlips, countFlips, flipACoin} from "./modules/coin.mjs"
+//import minimist from './node_modules/minimist'
 import minimist from 'minimist'
 
 let args = minimist(process.argv.slice(2));
@@ -6,16 +7,19 @@ let args = minimist(process.argv.slice(2));
 let theNum = args['number'];
 //args['number'];
 console.log(theNum);
-//args['theNum'];
+//THIS DOESNT YET WORK :(
+
+
 //let theNum = args.number;
 
+//I think this logic is ok if I could get input to work
 
-//if (theNum == null) {
-   // let flip = coinFlip(1);
-   // console.log(flip);
-  //  console.log(countFlips(flip));
-//} else {
-    //let flips = coinFlips(theNum);
-    //console.log(flips);
-  //  console.log(countFlips(flips))
-//}
+if (theNum == null || undefined) {
+    let flip = coinFlip(1);
+    console.log(flip);
+    console.log(countFlips(flip));
+} else {
+    let flips = coinFlips(theNum);
+    console.log(flips);
+    console.log(countFlips(flips))
+}
--- guess-flip.js
+++ guess-flip.js
@@ -1,2 +1,10 @@
 import {coinFlip, coinFlips, countFlips, flipACoin} from "./modules/coin.mjs"
+import minimist from 'minimist'
 
+//take one argument from cmd line named --call 
+//that will only accpet (heads or tails I think)
+
+//if no argument is given, or something other than
+//heads or tails, return an error message
+
+//compare the call with a flip and report the result to STDOUT
