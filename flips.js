dc89d4350bfe6af4d1b7d27cef17a9791aed5a1b
lamngo13 <lamngo@live.unc.edu>
2022-04-05 14:17:40 -0400

    trying for flips

--- flips.js
+++ flips.js
@@ -1,3 +1,12 @@
 import {coinFlip, coinFlips, countFlips, flipACoin} from "./modules/coin.mjs"
+import min from 'minimist'
 
-console.log(coinFlips);
+let args = min(process.argv.slice(2));
+
+if (args['number']) {
+    console.log(coinFlips(args['number']))
+    console.log(countFlips(coinFlips(args['number'])))
+} else {
+    console.log(coinFlips(1));
+    console.log(countFlips(coinFlip(1)));
+}
