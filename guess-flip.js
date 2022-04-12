175432ae526b9cbec10b81dff8bac41e42b45efe
lamngo13 <lamngo@live.unc.edu>
2022-04-12 12:47:27 -0400

    working on guess flips

--- guess-flip.js
+++ guess-flip.js
@@ -1,6 +1,13 @@
 import {coinFlip, coinFlips, countFlips, flipACoin} from "./modules/coin.mjs"
-import minimist from 'minimist'
 
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const args = require('minimist')(process.argv.slice(2))
+let theCall = args['call']
+
+//test
+//console.log(theCall)
 //take one argument from cmd line named --call 
 //that will only accpet (heads or tails I think)
 
@@ -8,3 +15,15 @@ import minimist from 'minimist'
 //heads or tails, return an error message
 
 //compare the call with a flip and report the result to STDOUT
+
+//IF NO ARG OR NOT HEADS OR TAILS, RETURN ERROR
+
+
+console.log(theCall)
+if (theCall==null ||theCall == undefined || ((theCall!="heads")&&(theCall!="tails"))) {
+    console.log("Error: no input.")
+} else {
+    let theFlip = flipACoin(theCall)
+    console.log(theFlip)
+}
+
